bc4b3490dada70ebf405aad6c60b854680ec8050
Tushar Thapliyal <tusharthapliyal8979@gmail.com>
2024-02-29 14:56:57 +0530

    added provider,main,variable

---
+++ provider.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+  region              = var.region
+  shared_config_files = ["/Users/tusharthapliyal/.aws/credentials"]
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "region" {
+  type        = string
+  description = "deafult region of aws"
+}
